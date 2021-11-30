648ac083522a6af0a9eabcb55646e957b96f4e22
zarathacia <ayabenslimen.aya195@gmail.com>
2021-11-30 18:45:52 +0100

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Shopili',
+      title: 'Shopily',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
