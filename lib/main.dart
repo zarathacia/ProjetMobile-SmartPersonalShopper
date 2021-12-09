3a2c619e00bbc0310bd115bf3ef8b2557dcc2c62
jiheddachraoui <jiheddachraoui@outlook.fr>
2021-12-09 10:29:57 +0100

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:smart_personal_shopper/constants.dart';
 import 'package:smart_personal_shopper/provider/feedback.dart';
 import 'package:smart_personal_shopper/screens/Landing/intro.dart';
-import 'package:smart_personal_shopper/screens/Landing/landingscreen.dart';
-
-import 'screens/home.dart';
 
 void main() {
   runApp(MyApp());
