88207f1b93d9fafa5d0694db16af4da6cc1a265d
jiheddachraoui <jiheddachraoui@outlook.fr>
2022-01-10 17:33:52 +0100

    fixing appbar

--- lib/widget/appbar.dart
+++ lib/widget/appbar.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:smart_personal_shopper/constants.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:smart_personal_shopper/constants.dart';
+import 'package:smart_personal_shopper/screens/Payment/paycard.dart';
+import 'package:smart_personal_shopper/screens/profile.dart';
 
 AppBar homeAppBar(BuildContext context) {
+  final ButtonStyle style =
+      TextButton.styleFrom(primary: Theme.of(context).colorScheme.primary);
   return AppBar(
     backgroundColor: Colors.white,
     elevation: 0,
@@ -27,10 +34,44 @@ AppBar homeAppBar(BuildContext context) {
       ),
     ),
     actions: <Widget>[
-      IconButton(
-        icon: SvgPicture.asset("assets/logos/notification.svg"),
-        onPressed: () {},
+      TextButton(
+        style: style,
+        onPressed: () {
+          //Navigator.of(context).push(MaterialPageRoute(builder: (context) => MyCard()));
+          AwesomeDialog(
+            context: context,
+            headerAnimationLoop: false,
+            dialogType: DialogType.NO_HEADER,
+            title: 'Your have 180.52 DT',
+            desc: 'Do you want to recharge your account?',
+            btnCancelText: "No",
+            btnOkText: "Recharge",
+            btnCancelOnPress: () {},
+            btnOkOnPress: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => MyCard()));
+            },
+            btnOkColor: SecondaryRed,
+            btnCancelColor: Colors.grey[500],
+          )..show();
+        },
+        child: const Text('Credit:180.52 DT'),
       ),
+      TextButton(
+          style: style,
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => Profile()));
+          },
+          child: new Container(
+            width: 70.0,
+            height: 45.0,
+            decoration: new BoxDecoration(
+                shape: BoxShape.circle,
+                image: new DecorationImage(
+                    fit: BoxFit.contain,
+                    image: AssetImage('images/pouya.jfif'))),
+          ))
     ],
   );
 }
